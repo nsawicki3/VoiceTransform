8f7d1117a1deb8df3a78dbf460260f30303d0aad
nsawicki <nathan.sawicki3@gmail.com>
2021-06-16 18:06:43 -0700

    Removed Old Matlab Files

--- Scripts/findMaxCorr.m
+++
@@ -1,8 +0,0 @@
-function shift_amount = findMaxCorr(clip1,clip2)
-
-clip1_spectro = spectrogram(clip1,blackman(44100),10000,44100,44100,'yaxis');
-clip2_spectro = spectrogram(clip2,blackman(44100),10000,44100,44100,'yaxis');
-
-C = conv2(clip1_spectro,clip2_spectro);
-image(abs(C));
-[val,shift_amount] = max(abs(C(1,:)));
--- Scripts/sound2.m
+++
@@ -1,6 +0,0 @@
-function sound2(clip1,clip2,fs)
-
-min_length = min(size(clip1,1),size(clip2,1));
-plot(clip1(1:1:min_length,:) + clip2(1:1:min_length,:));
-sound(clip1(1:1:min_length,:) + clip2(1:1:min_length,:),fs)
-
