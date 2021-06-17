55507724e5f410a21992e712b706aaeb3ae96632
nsawicki <nathan.sawicki3@gmail.com>
2021-06-16 18:13:01 -0700

    Initial Voice Transform class commit

---
+++ VoiceTransform/VoiceTransform.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+
+class VoiceTransform:
+
+    def __init__(self):
+
+        print('Initializing Voice Transform...')
+
+
+
+if __name__ == '__main__':
+
+    VT = VoiceTransform()
